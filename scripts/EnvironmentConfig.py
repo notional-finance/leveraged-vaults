869ccdfaac60b7d1fee4b0216ffdbaf87b167854
Tianjie Wei <weitianjie@gmail.com>
2022-06-17 11:37:41 -0700

    Adding vault controller to mainnet fork scripts

--- scripts/EnvironmentConfig.py
+++ scripts/EnvironmentConfig.py
@@ -1,7 +1,8 @@
 import json
 
-from brownie import accounts
+from brownie import accounts, network
 from brownie.network.contract import Contract
+from scripts.common import deployArtifact
 
 with open("abi/nComptroller.json", "r") as a:
     Comptroller = json.load(a)
@@ -32,17 +33,20 @@ class Environment:
     def __init__(self, network) -> None:
         self.network = network
         addresses = networks[network]
-
+        self.addresses = addresses
+        self.deployer = accounts.at(addresses["deployer"], force=True)
         self.notional = Contract.from_abi(
             "Notional", addresses["notional"], NotionalABI
         )
 
+        self.upgradeNotional()
+
         self.tokens = {}
         for (symbol, obj) in addresses["tokens"].items():
             if symbol.startswith("c"):
-                self.tokens[symbol] = Contract.from_abi(symbol, obj['address'], cToken["abi"])
+                self.tokens[symbol] = Contract.from_abi(symbol, obj, cToken["abi"])
             else:
-                self.tokens[symbol] = Contract.from_abi(symbol, obj['address'], ERC20ABI)
+                self.tokens[symbol] = Contract.from_abi(symbol, obj, ERC20ABI)
 
         self.whales = {}
         for (name, addr) in addresses["whales"].items():
@@ -50,6 +54,52 @@ class Environment:
 
         self.owner = accounts.at(self.notional.owner(), force=True)
 
+    def upgradeNotional(self):
+        tradingAction = deployArtifact(
+            "scripts/artifacts/TradingAction.json", 
+            [], 
+            self.deployer, 
+            "TradingAction", 
+            {"SettleAssetsExternal": "0x01713633a1b85a4a3d2f9430C68Bd4392c4a90eA"}
+        )
+        vaultAccountAction = deployArtifact(
+            "scripts/artifacts/VaultAccountAction.json", 
+            [], 
+            self.deployer, 
+            "VaultAccountAction", 
+            {"TradingAction": tradingAction.address}
+        )
+        vaultAction = deployArtifact(
+            "scripts/artifacts/VaultAction.json", 
+            [], 
+            self.deployer, 
+            "VaultAction",  
+            {"TradingAction": tradingAction.address})
+        router = deployArtifact("scripts/artifacts/Router.json", [
+            (
+                self.addresses["actions"]["GovernanceAction"],
+                self.addresses["actions"]["Views"],
+                self.addresses["actions"]["InitializeMarketsAction"],
+                self.addresses["actions"]["nTokenAction"],
+                self.addresses["actions"]["BatchAction"],
+                self.addresses["actions"]["AccountAction"],
+                self.addresses["actions"]["ERC1155Action"],
+                self.addresses["actions"]["LiquidateCurrencyAction"],
+                self.addresses["actions"]["LiquidatefCashAction"],
+                self.addresses["tokens"]["cETH"],
+                self.addresses["actions"]["TreasuryAction"],
+                self.addresses["actions"]["CalculationViews"],
+                vaultAccountAction.address,
+                vaultAction.address,
+            )
+        ], self.deployer, "Router", {})
+        self.notional.upgradeTo(router.address, {'from': self.notional.owner()})
 
 def getEnvironment(network = "mainnet"):
     return Environment(network)
+
+def main():
+    networkName = network.show_active()
+    if networkName == "hardhat-fork":
+        networkName = "mainnet"
+    env = Environment(networkName)
