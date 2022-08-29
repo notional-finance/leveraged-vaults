22a682194c99e2d229db6950b75c58c7022847ff
Jeff Wu <jeffywu@pm.me>
2022-08-29 10:45:03 -0700

    adding dev comment

--- contracts/trading/adapters/ZeroExAdapter.sol
+++ contracts/trading/adapters/ZeroExAdapter.sol
@@ -5,6 +5,10 @@ import {Deployments} from "../../global/Deployments.sol";
 import {Trade} from "../../../interfaces/trading/ITradingModule.sol";
 
 library ZeroExAdapter {
+    /// @dev executeTrade validates pre and post trade balances and also
+    /// sets and revokes all approvals. We are also only calling a trusted
+    /// zero ex proxy in this case. Therefore no order validation is done
+    /// to allow for flexibility.
     function getExecutionData(address from, Trade calldata trade)
         internal view returns (
             address spender,
