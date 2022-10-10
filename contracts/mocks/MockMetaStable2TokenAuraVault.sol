0ab57424d6d5586a0248b44dcc48c007bd05be0a
Tianjie Wei <weitianjie@gmail.com>
2022-10-10 12:11:23 -0700

    Adding mocks

--- contracts/mocks/MockMetaStable2TokenAuraVault.sol
+++ contracts/mocks/MockMetaStable2TokenAuraVault.sol
@@ -6,10 +6,16 @@ import {MetaStable2TokenAuraVault} from "../vaults/MetaStable2TokenAuraVault.sol
 import {NotionalProxy} from "../../interfaces/notional/NotionalProxy.sol";
 
 contract MockMetaStable2TokenAuraVault is MetaStable2TokenAuraVault {
+    uint256 public valuationFactor;
+
     constructor(
         NotionalProxy notional_, 
         AuraVaultDeploymentParams memory params
     ) MetaStable2TokenAuraVault(notional_, params) {
+    }
+
+    function setValuationFactor(uint256 valuationFactor_) external {
+        valuationFactor = valuationFactor_;
     }
 
     function convertStrategyToUnderlying(
@@ -17,6 +23,7 @@ contract MockMetaStable2TokenAuraVault is MetaStable2TokenAuraVault {
         uint256 strategyTokenAmount,
         uint256 maturity
     ) public view override returns (int256 underlyingValue) {
-        return super.convertStrategyToUnderlying(account, strategyTokenAmount, maturity);
+        underlyingValue = super.convertStrategyToUnderlying(account, strategyTokenAmount, maturity);
+        underlyingValue = underlyingValue * int256(valuationFactor) / 1e8;
     }
 }
