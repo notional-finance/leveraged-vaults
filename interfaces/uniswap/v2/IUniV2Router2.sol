429c25742944e3db44577c64caac573c8d025cd2
Tianjie Wei <weitianjie@gmail.com>
2022-10-20 10:52:24 -0700

    fix: sherlock-audit/2022-09-notional-judging#33

--- interfaces/uniswap/v2/IUniV2Router2.sol
+++ interfaces/uniswap/v2/IUniV2Router2.sol
@@ -3,6 +3,36 @@ pragma solidity 0.8.15;
 pragma abicoder v2;
 
 interface IUniV2Router2 {
+    function swapETHForExactTokens(
+        uint256 amountOut,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external payable returns (uint256[] memory amounts);
+
+    function swapExactETHForTokens(
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external payable returns (uint256[] memory amounts);
+
+    function swapTokensForExactETH(
+        uint256 amountOut,
+        uint256 amountInMax,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
+    function swapExactTokensForETH(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
     function swapExactTokensForTokens(
         uint256 amountIn,
         uint256 amountOutMin,
