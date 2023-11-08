20d5e007d720e2909a6ef0fd28103c817a765b67
Jeff Wu <jeff@notional.finance>
2023-11-08 15:48:27 -0800

    fix: rearranging vault folder structure

---
+++ tests/SingleSidedLP/BaseComposablePool.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "./BaseSingleSidedLPVault.sol";
+import "../../contracts/vaults/BalancerComposableAuraVault.sol";
+import "../../contracts/vaults/balancer/mixins/AuraStakingMixin.sol";
+import "../../interfaces/balancer/IBalancerPool.sol";
+import "../../contracts/trading/adapters/BalancerV2Adapter.sol";
+
+abstract contract BaseComposablePool is BaseSingleSidedLPVault {
+    bytes32 balancerPoolId;
+    BalancerSpotPrice spotPrice;
+
+    function setUp() public override virtual {
+        // BAL on Arbitrum
+        rewardToken = IERC20(0x040d1EdC9569d4Bab2D15287Dc5A4F10F56a56B8);
+        super.setUp();
+    }
+
+    function deployVault() internal override returns (IStrategyVault) {
+        spotPrice = new BalancerSpotPrice();
+        poolToken = IERC20(IAuraRewardPool(address(rewardPool)).asset());
+        balancerPoolId = IBalancerPool(address(poolToken)).getPoolId();
+
+        IStrategyVault impl = new BalancerComposableAuraVault(
+            NOTIONAL, AuraVaultDeploymentParams({
+                rewardPool: IAuraRewardPool(address(rewardPool)),
+                baseParams: DeploymentParams({
+                    primaryBorrowCurrencyId: primaryBorrowCurrency,
+                    balancerPoolId: balancerPoolId,
+                    tradingModule: TRADING_MODULE
+                })
+            }),
+            spotPrice
+        );
+
+        bytes memory initData = abi.encodeWithSelector(
+            ISingleSidedLPStrategyVault.initialize.selector, InitParams({
+                name: "Vault",
+                borrowCurrencyId: primaryBorrowCurrency,
+                settings: settings
+            })
+        );
+
+        vm.prank(NOTIONAL.owner());
+        nProxy proxy = new nProxy(address(impl), initData);
+
+        // NOTE: no token permissions set, single sided join by default
+        return IStrategyVault(address(proxy));
+    }
+}
