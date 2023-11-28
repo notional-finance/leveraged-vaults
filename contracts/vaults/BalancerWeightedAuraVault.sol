3703e1a1a11e5380f1a15255c36ce062c8df1d1e
Jeff Wu <jeff@notional.finance>
2023-11-28 13:06:04 -0800

    fix: sherlock-audit/2023-10-notional-judging#111

--- contracts/vaults/BalancerWeightedAuraVault.sol
+++ contracts/vaults/BalancerWeightedAuraVault.sol
@@ -74,20 +74,12 @@ contract BalancerWeightedAuraVault is AuraStakingMixin {
     }
  
     function _checkPriceAndCalculateValue() internal view override returns (uint256) {
-        (/* */, uint8[] memory decimals) = TOKENS();
-
+        // Spot prices are returned in POOL_PRECISION decimals.
         (uint256[] memory balances, uint256[] memory spotPrices) = SPOT_PRICE.getWeightedSpotPrices(
             BALANCER_POOL_ID,
             address(BALANCER_POOL_TOKEN),
-            PRIMARY_INDEX(),
-            decimals[PRIMARY_INDEX()]
+            PRIMARY_INDEX()
         );
-
-        // Spot prices are returned in native decimals, convert them all to POOL_PRECISION
-        // as required in the _calculateLPTokenValue method.
-        for (uint256 i; i < spotPrices.length; i++) {
-            spotPrices[i] = spotPrices[i] * POOL_PRECISION() / 10 ** decimals[i];
-        }
 
         return _calculateLPTokenValue(balances, spotPrices);
     }
