868af4f33daacd669ec5eee5a050ee0a3c79e078
Jeff Wu <jeff@notional.finance>
2023-12-07 13:34:01 -0800

    fix: deploy new convex vault

--- scripts/deployVault.sh
+++ scripts/deployVault.sh
@@ -6,7 +6,7 @@ export UPGRADE_VAULT=false
 export UPDATE_CONFIG=true
 
 source .env
-forge script tests/SingleSidedLP/pools/USDC_DAI_USDT_USDC_e.t.sol:Test_USDC \
+forge script tests/SingleSidedLP/pools/FRAX_USDC_e.t.sol:Test_FRAX \
     -f $RPC_URL --sender 0x8F5ea3CDe898B208280c0e93F3aDaaf1F5c35a7e \
     --gas-limit 1125899906842624 --chain 42161 --account ARBITRUM-ONE_DEPLOYER --broadcast --verify
 
@@ -14,6 +14,7 @@ forge script tests/SingleSidedLP/pools/USDC_DAI_USDT_USDC_e.t.sol:Test_USDC \
 # send the transaction. Verification will not be required if the code has not changed.
 # cast send --account ARBITRUM-ONE_DEPLOYER --chain 42161  --gas-limit 120935302 --gas-price 0.1gwei --create \
 
-# forge verify-contract 0x740a42dB721F2118b472C419D49c6A627079B469 \
-#     contracts/vaults/BalancerComposableAuraVault.sol:BalancerComposableAuraVault -c 42161 \
+
+# forge verify-contract 0xA8D35f56587e0efF95Ff9e2Ec1Eb2124Fef31D8B \
+#     contracts/vaults/Curve2TokenConvexVault.sol:Curve2TokenConvexVault -c 42161 \
 #     --show-standard-json-input > json-input.std.json
