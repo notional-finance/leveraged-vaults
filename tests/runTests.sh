aff108321dc4001c3a283671b243e19063d82294
Jeff Wu <jeff@notional.finance>
2024-03-27 07:24:40 -0700

    fix: most deleverage tests passing

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -3,7 +3,7 @@ source .env
 python tests/SingleSidedLP/generate_tests.py
 
 export RPC_URL=$MAINNET_RPC_URL
-export FORK_BLOCK=19469130
+export FORK_BLOCK=19526000
 export FOUNDRY_PROFILE=mainnet
 forge build --force
 forge test --mp "tests/generated/mainnet/**"
@@ -14,5 +14,4 @@ export FOUNDRY_PROFILE=arbitrum
 forge build --force
 forge test --mp "tests/generated/arbitrum/**"
 
-forge test --mp "tests/testFlashLiquidator.t.sol"
-forge test --mp "tests/testTradingModule.t.sol"
+# forge test --mp "tests/testTradingModule.t.sol"
