4749fe708827a94721212f024655f47eef5a7704
Jeff Wu <jeff@notional.finance>
2024-04-22 10:50:09 -0700

    fix: update crvUSD configs

--- tests/generated/arbitrum/SingleSidedLP_Convex_crvUSD_xUSDT.t.sol
+++ tests/generated/arbitrum/SingleSidedLP_Convex_crvUSD_xUSDT.t.sol
@@ -33,13 +33,13 @@ Curve2TokenHarness
         params.liquidationRate = 103;
         params.reserveFeeShare = 80;
         params.maxBorrowMarketIndex = 2;
-        params.minCollateralRatioBPS = 500;
+        params.minCollateralRatioBPS = 1400;
         params.maxRequiredAccountCollateralRatioBPS = 10000;
-        params.maxDeleverageCollateralRatioBPS = 800;
+        params.maxDeleverageCollateralRatioBPS = 2600;
 
         // NOTE: these are always in 8 decimals
-        params.minAccountBorrowSize = 1e8;
-        maxPrimaryBorrow = 5_000e8;
+        params.minAccountBorrowSize = 5_000e8;
+        maxPrimaryBorrow = 500_000e8;
     }
 
     function getRequiredOracles() public override pure returns (
