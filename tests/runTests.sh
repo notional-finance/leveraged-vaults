d8e527dbf166bfde27ff6807ea3d415069e0771a
sbuljac <sbuljac@gmail.com>
2024-05-28 13:31:09 +0200

    remove flag

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -10,6 +10,6 @@ forge test --mp "tests/generated/mainnet/**"
 export RPC_URL=$ARBITRUM_RPC_URL
 export FORK_BLOCK=194820300
 export FOUNDRY_PROFILE=arbitrum
-forge test --mp "tests/generated/arbitrum/**" -vvvv
+forge test --mp "tests/generated/arbitrum/**" 
 
- forge test --mp "tests/testTradingModule.t.sol"
+forge test --mp "tests/testTradingModule.t.sol"
