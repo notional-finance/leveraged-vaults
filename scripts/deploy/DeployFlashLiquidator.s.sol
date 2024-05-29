055161e71078d8546a43a295b41674c0ce71694e
Jeff Wu <jeff@notional.finance>
2024-05-29 10:42:33 -0700

    fix: update liquidator deployment script

--- scripts/deploy/DeployFlashLiquidator.s.sol
+++ scripts/deploy/DeployFlashLiquidator.s.sol
@@ -7,11 +7,14 @@ import "@contracts/liquidator/FlashLiquidator.sol";
 
 contract DeployFlashLiquidator is Script {
     function run() public {
+        uint256 numCopies = vm.envOr("NUM_LIQUIDATORS", uint256(1));
         require(block.chainid == Deployments.CHAIN_ID, "Invalid Chain");
 
-        vm.startBroadcast();
-        address impl = address(new FlashLiquidator());
-        vm.stopBroadcast();
-        console.log("New Liquidator", impl);
+        for (uint256 i; i < numCopies; i++) {
+            vm.startBroadcast();
+            address impl = address(new FlashLiquidator());
+            vm.stopBroadcast();
+            console.log("New Liquidator", impl);
+        }
     }
 }
--- scripts/deployLiquidator.sh
+++ scripts/deployLiquidator.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 source .env
 
-export RPC_URL=$ARBITRUM_RPC_URL
-export FOUNDRY_PROFILE=arbitrum
-export CHAIN_ID=42161
-export DEPLOYER=MAINNET_V2_DEPLOYER
-export DEPLOYER_ADDRESS=`cast wallet address --account $DEPLOYER`
+export CHAIN_ID=arbitrum
 
-# forge build --force
+export FOUNDRY_PROFILE=$CHAIN_ID
+export DEPLOYER=MAINNET_V2_DEPLOYER
+RPC_VAR="$(echo "$CHAIN_ID"_RPC_URL | tr '[:lower:]' '[:upper:]')"
+export ETH_RPC_URL="${!RPC_VAR}"
+export DEPLOYER_ADDRESS=`cast wallet address --account $DEPLOYER`
+export NUM_LIQUIDATORS=2
+
 forge script scripts/deploy/DeployFlashLiquidator.s.sol:DeployFlashLiquidator \
-     -f $RPC_URL --sender $DEPLOYER_ADDRESS --account $DEPLOYER --verify --broadcast \
+     -f $ETH_RPC_URL --sender $DEPLOYER_ADDRESS --account $DEPLOYER --verify --broadcast \
      --verifier-url https://api.arbiscan.io/api --etherscan-api-key $ARBISCAN_API_KEY
+
+# Arbitrum Liquidators
+# 0xA44a8729d139b39A322a9c7754fAe98B6cff6C71
+# 0x53423Db7ac663Aa1941a809A6D787bfFc7A5C8A9
+# 0x0158fC072Ff5DDE8F7b9E2D00e8782093db888Db
